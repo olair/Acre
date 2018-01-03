b103504c4b588d98bf3d0dbb749402071d069553
olair <ddanyang44@gmail.com>
2018-01-03 23:16:18 +0800

    AcreView : 创建AcreView(用于展示土地形状).

--- app/src/main/java/cn/olair/view/AcreView.java
+++ app/src/main/java/cn/olair/view/AcreView.java
@@ -1,10 +1,13 @@
 package cn.olair.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.view.DragEvent;
 import android.view.View;
 
 /**
@@ -14,6 +17,8 @@ import android.view.View;
  */
 
 public class AcreView extends View {
+
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public AcreView(Context context) {
         super(context, null, 0);
@@ -31,4 +36,15 @@ public class AcreView extends View {
     public AcreView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        int radius = Math.min(width, height) / 2;
+        mPaint.setColor(0xFFFF0000);
+        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+    }
+
 }
