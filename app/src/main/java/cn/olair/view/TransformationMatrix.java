3cff95c59c3569bb87f021c68a8eb1db381189c0
Du Danyang <dudy@rd.wondershare.cn>
2018-03-12 18:27:40 +0800

    开始实现Matrix封装类。

--- app/src/main/java/cn/olair/view/TransformationMatrix.java
+++ app/src/main/java/cn/olair/view/TransformationMatrix.java
@@ -1,6 +1,8 @@
 package cn.olair.view;
 
 import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.RectF;
 
 /**
  * 封装Matrix的计算过程.
@@ -10,12 +12,43 @@ import android.graphics.Matrix;
 
 public class TransformationMatrix {
 
-    private int maxWidth;
-    private int maxHeight;
+    private RectF displayRect = new RectF();
+    private RectF contentRect = new RectF();
+    private RectF cacheRect = new RectF();
 
     Matrix mMatrix = new Matrix();
 
     TransformationMatrix(int maxWidth, int maxHeight) {
+        displayRect.set(0, 0, maxWidth, maxHeight);
+    }
+
+    public void addPoint(PointF point) {
+        contentRect.union(point.x, point.y);
+    }
+
+    public void calculate() {
+
+        float displayWidth = displayRect.width();
+        float displayHeight = displayRect.height();
+
+        float contentWidth = contentRect.width();
+        float contentHeight = contentRect.height();
+
+        float widthRatio = displayWidth/contentWidth;
+        float heightRatio = displayHeight / contentHeight;
+
+        float currentRatio = widthRatio < heightRatio ? widthRatio : heightRatio;
+
+        // ? 是否需要等于1
+        if (currentRatio >= 1 && !displayRect.contains(contentRect)) {
+            // 需要进行平移操作
+            cacheRect.setIntersect(displayRect, contentRect);
+            // 开始比较
+
+
+        } else {
+            // 涉及缩放以及平移
+        }
 
     }
 
