b5d42f61f07ebb2d7e4cd21284a079bc298ef050
Du Danyang <dudy@rd.wondershare.cn>
2018-03-12 17:46:24 +0800

    创建Matrix封装类。

---
+++ app/src/main/java/cn/olair/view/TransformationMatrix.java
@@ -0,0 +1,22 @@
+package cn.olair.view;
+
+import android.graphics.Matrix;
+
+/**
+ * 封装Matrix的计算过程.
+ *
+ * Created by ws on 2018/3/12.
+ */
+
+public class TransformationMatrix {
+
+    private int maxWidth;
+    private int maxHeight;
+
+    Matrix mMatrix = new Matrix();
+
+    TransformationMatrix(int maxWidth, int maxHeight) {
+
+    }
+
+}
